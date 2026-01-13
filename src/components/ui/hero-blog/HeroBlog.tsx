7c435da6c9072127c61f7b7e97ad2f35d43e37fd
ManuL0129 <manumonla@hotmai.com>
2026-01-12 23:49:19 -0500

    feat: implement HeroBlog component layout with image and category details

--- src/components/ui/hero-blog/HeroBlog.tsx
+++ src/components/ui/hero-blog/HeroBlog.tsx
@@ -22,5 +22,32 @@ export default function HeroBlog({
   createdAt,
   url,
 }: HeroBlogComponentProps) {
-  return <div></div>;
+  return (
+    <div>
+      <section className="relative w-full h-150 py-24 flex items-end justify-center overflow-hidden">
+        <img
+          src={url}
+          alt={blogName}
+          className="absolute inset-0 w-full h-full object-cover z-0"
+        />
+
+        <div className="flex flex-col  z-20 max-w-4xl text-left px-6">
+          <div className="inline-flex items-center gap-2 px-4 py-1 mb-6 text-xs font-semibold tracking-wide">
+            <div className=" px-3 py-1 rounded-md bg-blue-500 text-white ">
+              <span className="w-2 h-2 bg-green-400 rounded-full"></span>
+              {category.name}
+            </div>
+            <div className="text-white font-inter">
+              {" "}
+              {createdAt} · {timeline} lectura
+            </div>
+          </div>
+
+          <h1 className="text-4xl md:text-6xl font-extrabold text-white leading-tight">
+            {blogName} holaaaaaaa askhasjkasas
+          </h1>
+        </div>
+      </section>
+    </div>
+  );
 }
