c1d3dd98fdd9bc481d23d7d37d3ce48d5ed7417f
ManuL0129 <manumonla@hotmai.com>
2026-01-12 23:11:07 -0500

    feat: refactor Details component layout for improved presentation

--- src/components/ui/details/Details.tsx
+++ src/components/ui/details/Details.tsx
@@ -9,16 +9,18 @@ export interface DetailsProps {
 
 export default function Details({ details }: DetailsProps) {
   return (
-    <div className="flex justify-center items-center">
-      <div className="grid grid-cols-4 gap-50">
-        {details.map((detail, index) => (
-          <div key={index} className="flex flex-col items-center">
-            <p className="text-gray-500">{detail.content}</p>
-            <h3 className="text-lg font-semibold text-gray-900">
-              {detail.header}
-            </h3>
-          </div>
-        ))}
+    <div className="flex justify-center items-center ">
+      <div className="border px-80 border-gray-200 shadow-sm ">
+        <div className="flex gap-50 py-8">
+          {details.map((detail, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <p className="text-gray-500">{detail.content}</p>
+              <h3 className="text-lg font-semibold text-gray-900">
+                {detail.header}
+              </h3>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
