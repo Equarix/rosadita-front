e1860ef08132326c672529ba43b10c23b15d1c9a
Laszlo-Caballero <laszlocaballero@gmail.com>
2026-08-14 09:56:20 -0500

    feat: implement blog landing page with hero, search, and footer components including full internationalization support

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -10,7 +10,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   }
 
 
-  const files = ["common", "home", "projects", "services"];
+  const files = ["common", "home", "projects", "services", "blog"];
 
   const messagesEntries = await Promise.all(
     files.map(async (file) => {
